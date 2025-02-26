4ac5100552189c11c98ea9fb0278dcab53b0a5d2
David Ihnen <david.ihnen@omnifederal.com>
2025-02-26 09:40:24 -0500

    more ideas

--- src/index.ts
+++ src/index.ts
@@ -28,30 +28,89 @@ const classifiers: ((row: Row) => Promise<Classification>)[] = [
     // if you find an (update that changes parent to a parent that does not exist)
     // then if (there is a create that sets parent to the update's before parent shortly before)
     // the identifier of the create is the after parent of the update
+    async (row: Row) => {
+        if (row.mode !== 'UPDATE') return {aok: true}
+        if (row.before.parent === row.after.parent) return {aok: true}
+        if (!row.before.parent) return {aok: true}
+        if (!row.after.parent) return {aok: true}
 
-   // if there is an update or destroy without an identifier
-   // look previous in time to look for CREATE with an AFTER name that matches the update ord estroys BEFORE name
+        console.log(`checking ${row.id}`)
+
+        return sql`SELECT
+                       p.id,
+                       os.after->>'name' AS createName,
+                       os.created AS createTime,
+                       p.created AS updateTime,
+                       p.before->>'name' AS before,
+                       p.after->>'name' AS after,
+                       p.before->>'parent' AS prev_parent_of_change,
+                       p.after->>'parent' AS missing_identifier,
+                       os.id AS id_getting_identifier
+                   FROM
+                       clade_transactions p
+                       JOIN
+                       clade_transactions os ON os.after->>'parent' = p.before->>'parent'
+                   WHERE
+                       p.id = ${row.id}
+                     AND p.mode = 'UPDATE'
+                     AND p.before->>'parent' != p.after->>'parent'
+                     AND os.mode = 'CREATE'
+                     AND p.created > os.created
+                     AND NOT EXISTS (
+                       SELECT 1
+                       FROM clade_transactions checkMissingAfterParent
+                       WHERE checkMissingAfterParent.identifier = p.after->>'parent'
+                     AND checkMissingAfterParent.mode = 'CREATE'
+                       )
+                     AND NOT EXISTS (
+                       SELECT 1
+                       FROM clade_transactions os2
+                       WHERE os2.identifier = os.identifier
+                       )
+                   ORDER BY
+                       os.created;
+        `
+            .then(rowList => {
+                if (rowList.length <= 0 ) {
+                    return {aok: true}
+                }
+                console.log(`${row.id} update row match needs identifier`, rowList)
+
+                return {
+                    aok: false, propose: () => {
+                        console.log("------ Found a parent change insert pattern missing identifier ---", rowList)
+                    }
+                }
+            }).catch(m => {
+                console.log(m)
+                return {aok: false, why: m}
+            })
+    },
+
+    // if there is an update or destroy without an identifier
+    // look previous in time to look for CREATE with an AFTER name that matches the update ord estroys BEFORE name
     // and propose the CREATE identifier is the BEFORE identifier
 
     // there is an id missing
     async (row: Row) => {
+        throw false
         if (!row.identifier) {
             if (row.mode == "CREATE") {
                 console.log(`create row without identifier, scanning for ${row.after.name} in a before->name`)
                 sql`SELECT identifier, count(*), max('user') as upuser, min('user') as downuser
-                    FROM transactions_old
-                    WHERE before->'name' = ${row.after.name}
-                    AND before->'parent' = ${row.after.parent}
-                    AND identifier IS NOT NULL
+                    FROM clade_transactions
+                    WHERE before - > 'name' = ${row.after.name}
+                      AND before - > 'parent' = ${row.after.parent}
+                      AND identifier IS NOT NULL
                     GROUP BY identifier`
                     .then(matches => {
                             if (matches.length > 1) {
                                 console.log("we have multiple identifiers: ", matches)
                                 sql`SELECT *
-                                    FROM transactions_old
+                                    FROM clade_transactions
                                     WHERE mode = "CREATE"
-                                    AND identifier in ('${matches.map(r => r.identifier).join("','")}')
-                                    `.then(multimatches => {
+                                      AND identifier in ('${matches.map(r => r.identifier).join("','")}')
+                                `.then(multimatches => {
                                     console.log("create scan!", multimatches)
                                 })
                             }
@@ -83,8 +142,8 @@ const classifiers: ((row: Row) => Promise<Classification>)[] = [
                     // generate identifier
                     // find another transaction with the same before name
                     sql`SELECT *
-                        FROM transactions_old
-                        WHERE `
+                        FROM clade_transactions
+                        WHERE id = '5afef805a5cd280c816a2776'`
 
                     console.log(`${row.id} has no identifier, I found a candidate`)
                     const candidate = {identifier: "five"};
@@ -101,7 +160,7 @@ const classifiers: ((row: Row) => Promise<Classification>)[] = [
 ];
 
 sql`SELECT *
-    FROM transactions_old`.cursor(200, async a => {
+    FROM clade_transactions WHERE id not like '%-%'`.cursor(200, async a => {
     Promise.allSettled(a.flatMap(r => classifiers.map(c => c(r))))
         .then(psr => psr.filter(p => p.status == "fulfilled"))
         .then(ar => ar.map(psr => psr.value))
